43506cb07903c1a0f6b894793beb892377ce408a
Koudai Yamaguchi <serah.hound@gmail.com>
2023-04-05 01:11:01 +0900

    GitTestにgetter setterの追加

--- GitTest.java
+++ GitTest.java
@@ -7,4 +7,21 @@ public class GitTest {
 	public void testMethod() {
 		System.out.println("Githubのテストだよ");
 	}
+	
+	public String getName(){
+		return this.name;
+	}
+	public void setName(String n){
+		this.name = n;
+	}
+	
+	public int getAge(){
+		return this.name;
+	}
+	
+	public void setAge(int age){
+		this.age = age;
+	}
+	
+	
 }
